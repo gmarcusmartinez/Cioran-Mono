f77c039bffdd6c526a24268250c8766a43c9ea03
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-01-10 12:04:55 -0600

    Boilerplate react client w/ typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import Header from "./components/layout/Header";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <div id="header">
+      <h1>Cioran</h1>
+    </div>
+  );
+};
+export default Header;
