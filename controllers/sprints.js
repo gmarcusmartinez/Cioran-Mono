c7dbad87caf7045bc33b6a2e83a4a2972ff63c4f
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-01-14 13:56:29 -0600

    CRUD functionality for Sprint resources

--- controllers/sprints.js
+++ controllers/sprints.js
@@ -1,4 +1,5 @@
 const Sprint = require("../models/Sprint");
+const Project = require("../models/Project");
 const asyncHandler = require("../middleware/async");
 const ErrorResponse = require("../utils/errorResponse");
 
@@ -18,5 +19,69 @@ exports.getSprints = asyncHandler(async (req, res, next) => {
     success: true,
     count: sprints.length,
     data: sprints
+  });
+});
+exports.getSprint = asyncHandler(async (req, res, next) => {
+  const sprint = await Sprint.findById(req.params.id).populate({
+    path: "project",
+    select: "title"
+  });
+  if (!sprint) {
+    return next(
+      new ErrorResponse(`No sprint with id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: sprint
+  });
+});
+exports.createSprint = asyncHandler(async (req, res, next) => {
+  req.body.project = req.params.projectId;
+
+  const project = await Project.findById(req.params.projectId);
+  if (!project) {
+    return next(
+      new ErrorResponse(`No projects with id of ${req.params.projectId}`, 404)
+    );
+  }
+  const sprint = await Sprint.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: sprint
+  });
+});
+
+exports.updateSprint = asyncHandler(async (req, res, next) => {
+  let sprint = await Sprint.findById(req.params.id);
+  if (!sprint) {
+    return next(
+      new ErrorResponse(`No bootcamp with id of ${req.params.id}`, 404)
+    );
+  }
+  sprint = await Sprint.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: sprint
+  });
+});
+
+exports.deleteSprint = asyncHandler(async (req, res, next) => {
+  const sprint = await Sprint.findById(req.params.id);
+  if (!sprint) {
+    return next(
+      new ErrorResponse(`No sprint with id of ${req.params.id}`, 404)
+    );
+  }
+  await sprint.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {}
   });
 });
--- routes/sprints.js
+++ routes/sprints.js
@@ -1,9 +1,24 @@
 const { Router } = require("express");
 
-const { getSprints } = require("../controllers/sprints");
+const {
+  getSprints,
+  getSprint,
+  createSprint,
+  updateSprint,
+  deleteSprint
+} = require("../controllers/sprints");
 
 const router = Router({ mergeParams: true });
 
-router.route("/").get(getSprints);
+router
+  .route("/")
+  .get(getSprints)
+  .post(createSprint);
+
+router
+  .route("/:id")
+  .get(getSprint)
+  .put(updateSprint)
+  .delete(deleteSprint);
 
 module.exports = router;
