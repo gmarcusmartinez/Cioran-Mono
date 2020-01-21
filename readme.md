dd67605c1aea42cc2c71d6ae9942b0bac6718d5f
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-01-21 13:21:35 -0600

    readme.md added

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Cioran API
+
+> Api for an agile project manager for development teams.
+
+## Install Dependencies
+
+```
+npm install
+```
+
+## Run App
+
+```
+# Run in developmetn mode
+npm run dev
+```
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const hpp = require("hpp");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
@@ -6,6 +8,7 @@ const xss = require("xss-clean");
 const express = require("express");
 const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
+const rateLimit = require("express-rate-limit");
 const errorHandler = require("./middleware/error");
 const mongoSanitize = require("express-mongo-sanitize");
 
@@ -26,8 +29,16 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(mongoSanitize());
 app.use(helmet());
-// Prevent XSS attacks
+// Prevent Cross side scripting attacks
 app.use(xss());
+app.use(hpp());
+app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
 
 app.use("/api/auth", auth);
 app.use("/api/users", users);
