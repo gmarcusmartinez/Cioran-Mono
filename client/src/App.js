9e19aa286f074d85df6f51d48b6c359f394fa8e2
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-01-30 07:50:33 -0600

    Redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
 import "./App.scss";
+import React from "react";
+import store from "./store/store";
+import { Provider } from "react-redux";
+import { Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Landing from "./components/Landing/Landing";
@@ -8,13 +10,15 @@ import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Landing} />
-      </Switch>
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+        </Switch>
+        <Footer />
+      </div>
+    </Provider>
   );
 }
 
