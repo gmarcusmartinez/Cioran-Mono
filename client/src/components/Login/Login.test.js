7895d568b18eb0aca753e2c8fe67510a903ea60a
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-02-11 07:44:16 -0800

    setState tests for Login setFormData tests passing

--- client/src/components/Login/Login.test.js
+++ client/src/components/Login/Login.test.js
@@ -24,3 +24,34 @@ describe("Login", () => {
     expect(passwordInput.exists()).toBe(true);
   });
 });
+
+describe("state controlled login input fields", () => {
+  let wrapper;
+  let mockSetFormData;
+  beforeEach(() => {
+    mockSetFormData = jest.fn();
+    React.useState = jest.fn(() => ["", mockSetFormData]);
+    wrapper = setup();
+  });
+  test("state updates with value of email input upon change", () => {
+    const emailInput = findByTestAttr(wrapper, "email-input");
+    const emailInputMockEvent = {
+      target: { name: "email", value: "test@app.com" }
+    };
+
+    emailInput.simulate("change", emailInputMockEvent);
+    expect(mockSetFormData).toHaveBeenCalledWith({
+      email: "test@app.com"
+    });
+  });
+  test("state updates with value of of password input upon change", () => {
+    const passwordInput = findByTestAttr(wrapper, "password-input");
+    const passwordInputMockEvent = {
+      target: { name: "password", value: "testing123" }
+    };
+    passwordInput.simulate("change", passwordInputMockEvent);
+    expect(mockSetFormData).toHaveBeenCalledWith({
+      password: "testing123"
+    });
+  });
+});
